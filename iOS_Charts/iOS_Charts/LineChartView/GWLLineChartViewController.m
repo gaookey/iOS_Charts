90b520e6cac6e438bb569006c3cd6b5fa473f3dd
高文立 <gao.wenli@foxmail.com>
2020-02-01 17:29:46 +0800

    LineChartView

--- iOS_Charts/iOS_Charts/LineChartView/GWLLineChartViewController.m
+++ iOS_Charts/iOS_Charts/LineChartView/GWLLineChartViewController.m
@@ -8,7 +8,10 @@
 
 #import "GWLLineChartViewController.h"
 
-@interface GWLLineChartViewController ()
+@interface GWLLineChartViewController () <IChartAxisValueFormatter, ChartViewDelegate>
+
+@property (strong, nonatomic) LineChartView *lineChartView;
+@property (strong, nonatomic) NSArray *datas;
 
 @end
 
@@ -19,16 +22,40 @@
     
     self.title = @"LineChartView";
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.datas = @[@"23", @"13", @"53", @"63", @"35", @"39", @"70", @"23", @"13", @"53", @"63", @"35", @"39", @"70"];
+    
+    [self setupLineChartView];
+}
+#pragma mark - LineChartView
+-(void)setupLineChartView {
+    [self.view addSubview:self.lineChartView];
+    
+    [self drawData];
+}
+- (void)drawData {
+    NSMutableArray *values = [[NSMutableArray alloc] init];
+    for (int i = 0; i < self.datas.count; i++) {
+        NSString * aaa = self.datas[i];
+        double bb = aaa.doubleValue;
+        ChartDataEntry *entry = [[ChartDataEntry alloc] initWithX:i y:bb];
+        [values addObject:entry];
+    }
+    
+    LineChartDataSet *dataSet = [[LineChartDataSet alloc] initWithEntries:values label:@"图例"];
+    
+    LineChartData *data = [[LineChartData alloc] initWithDataSet:dataSet];
+    
+    self.lineChartView.data = data;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - lazy
+- (LineChartView *)lineChartView {
+    if (!_lineChartView) {
+        _lineChartView = [[LineChartView alloc] initWithFrame:CGRectMake(0, NAV_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - NAV_HEIGHT - TABBAR_BOTTOM_HEIGHT)];
+        _lineChartView.delegate = self;
+    }
+    return _lineChartView;
 }
-*/
 
 @end
